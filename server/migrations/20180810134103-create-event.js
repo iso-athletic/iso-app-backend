a81cca91a441259c9ec12b0f4efdcadec49fcb3c
Joe Cummings <joecummings@Joes-MacBook-Pro.local>
2018-08-10 16:29:54 -0500

    migrations

---
+++ server/migrations/20180810134103-create-event.js
@@ -0,0 +1,60 @@
+module.exports = {
+    up: (queryInterface, Sequelize) =>
+        queryInterface.createTable('Events', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            action_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'Actions',
+                    key: 'id',
+                    as: 'action_id',
+                },
+            },
+            player_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'Players',
+                    key: 'id',
+                    as: 'player_id',
+                },
+            },
+            team_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'Teams',
+                    key: 'id',
+                    as: 'team_id',
+                },
+            },
+            drill_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'Drills',
+                    key: 'id',
+                    as: 'drill_id',
+                },
+            },
+            location: {
+                type: Sequelize.JSON,
+            },
+            timestamp: {
+                type: Sequelize.DATE
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                field: "created_at"
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                field: "updated_at"
+            }
+        }),
+    down: (queryInterface, Sequelize) => queryInterface.dropTable('Events')
+}
