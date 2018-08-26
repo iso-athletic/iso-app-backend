ac9829ed290152c982e021933a5cf390a6749f8a
Joe Cummings <joecummings@Joes-MacBook-Pro.local>
2018-08-26 15:44:54 -0500

    adding cors

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 require('./server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
