2f65bff956114cd64d69f64c43f2e5ff830ee95c
Ethan Zack <ethanzack2021@u.northwestern.edu>
2019-04-24 16:12:00 -0500

    another buggy version

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -33,14 +33,16 @@ module.exports = {
           timestamp: {
             $between: [moment.unix(req.params.date_from).format('YYYY-MM-DD HH:MM:SS'), moment.unix(req.params.date_to).format('YYYY-MM-DD HH:MM:SS')]
           },
-          $or: [{action_id: "Made Shot"}, {action_id: "Missed Shot"}]
-        },
-        include: [{
-          model: Player,
-          where: {
-            organization_id: req.params.organization_id,
+          $or: [{action_id: "Made Shot"}, {action_id: "Missed Shot"}],
+          player_id: {
+            $in: {
+                include: [{
+                model: Player,
+                where: {organization_id: req.params.organization_id}
+              }]
+            }
           }
-        }]
+        }
       })
       .then(event=>res.status(201).send(event))
       .catch(error => res.status(400).send(error));
